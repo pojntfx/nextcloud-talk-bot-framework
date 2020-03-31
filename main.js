677968e7cc33fa6e0a8ea5d1cff5c67e7d4d2d3e
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-03-31 23:37:49 +0200

    feat: Add basic implementation

---
+++ main.js
@@ -0,0 +1,14 @@
+const Nextcloudbot = require("nextcloud-talk-bot");
+
+const bot = new Nextcloudbot({
+  autoJoin: true
+});
+
+bot.onText(/^\#(v|V)ideo(call|chat)$/, msg =>
+  msg.reply(
+    `@${msg.actorId} started a video call. Tap on ${process.env.NCTB_JITSI_URL}/${msg.token} to join!`,
+    false
+  )
+);
+
+bot.startPolling();
