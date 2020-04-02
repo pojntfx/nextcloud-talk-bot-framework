ce55346550adc5acddd7c5be07c8abf1c2927694
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-04-02 22:43:57 +0200

    feat: Start nextcloud talk decomposition

--- pkg/v2/pkg/client/room.go
+++ pkg/v2/pkg/client/room.go
@@ -1,12 +1,5 @@
 package client
 
-import (
-	"encoding/json"
-	"path"
-
-	"github.com/go-resty/resty/v2"
-)
-
 // Room is a chat room
 type Room struct {
 	ID          int    `json:"id"`
@@ -20,29 +13,4 @@ type RoomResponse struct {
 	OCS struct {
 		Data []Room `json:"data"`
 	} `json:"ocs"`
-}
-
-// GetRooms gets the chat rooms
-func GetRooms(url, username, password string) ([]Room, error) {
-	client := resty.New()
-
-	res, err := client.R().
-		SetHeaders(map[string]string{
-			"OCS-APIRequest": "true",
-			"Accept":         "application/json",
-		}).
-		SetBasicAuth(username, password).
-		Get(url + "/" + path.Join("ocs", "v2.php", "apps", "spreed", "api", "v1", "room"))
-	if err != nil {
-		return nil, err
-	}
-
-	var resStruct RoomResponse
-	if err := json.Unmarshal(res.Body(), &resStruct); err != nil {
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return resStruct.OCS.Data, nil
 }
