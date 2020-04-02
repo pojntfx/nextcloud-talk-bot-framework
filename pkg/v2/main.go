89aee768e1cd000c0fcd86a4dc3b2bfbd75ac2aa
Felix Pojtinger <felix@pojtinger.com>
2020-04-02 22:43:57 +0200

    feat: Start nextcloud talk decomposition

--- pkg/v2/main.go
+++ pkg/v2/main.go
@@ -5,10 +5,7 @@ import (
 	"log"
 	"os"
 	"regexp"
-	"strconv"
-	"time"
 
-	"github.com/akrylysov/pogreb"
 	"github.com/pojntfx/nextcloud-talk-jitsi-bot/pkg/v2/pkg/client"
 )
 
@@ -21,87 +18,21 @@ func main() {
 		jitsiURL   = os.Getenv("JITSI_URL")
 	)
 
-	roomChan := make(chan client.Room)
-	go func() {
-		var lastRooms []client.Room
+	chatChan, statusChan := make(chan client.Chat), make(chan string)
+	bot := client.NewNextcloudTalk(url, username, password, dbLocation, chatChan, statusChan)
 
-		for {
-			rooms, err := client.GetRooms(url, username, password)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			for _, room := range rooms {
-				exists := false
-
-				for _, lastRoom := range lastRooms {
-					if room.ID == lastRoom.ID {
-						exists = true
-
-						break
-					}
-				}
-
-				if !exists {
-					roomChan <- room
-				}
-			}
-
-			lastRooms = rooms
-
-			time.Sleep(time.Second * 5)
-		}
-	}()
-
-	knownIDs, err := pogreb.Open(dbLocation, nil)
-	if err != nil {
+	defer bot.Close()
+	if err := bot.Open(); err != nil {
 		log.Fatal(err)
 	}
-	defer knownIDs.Close()
 
-	chatChan := make(chan client.Chat)
+	if err := bot.Read(); err != nil {
+		log.Fatal(err)
+	}
+
 	go func() {
-		for room := range roomChan {
-			log.Printf(`Joined room "%v" ("%v") with ID "%v" and token "%v"`, room.DisplayName, room.Name, room.ID, room.Token)
-
-			go func(currentRoom client.Room) {
-				for {
-					lastID := []byte{}
-					has, err := knownIDs.Has([]byte(currentRoom.Token))
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					if has {
-						lastID, err = knownIDs.Get([]byte(currentRoom.Token))
-						if err != nil {
-							log.Fatal(err)
-						}
-					}
-
-					chats, err := client.GetChats(url, username, password, currentRoom.Token)
-					if err != nil {
-						if err.Error() == "invalid character '<' looking for beginning of value" {
-							log.Printf(`Left room "%v" ("%v") with ID "%v" and token "%v"`, currentRoom.DisplayName, currentRoom.Name, currentRoom.ID, currentRoom.Token)
-
-							return
-						}
-
-						log.Fatal(err)
-					}
-
-					chat := chats[0]
-					if strconv.Itoa(chat.ID) != string(lastID) {
-						chatChan <- chats[0]
-
-						if err := knownIDs.Put([]byte(currentRoom.Token), []byte(strconv.Itoa(chat.ID))); err != nil {
-							log.Fatal(err)
-						}
-					}
-
-					time.Sleep(time.Second * 5)
-				}
-			}(room)
+		for status := range statusChan {
+			log.Println(status)
 		}
 	}()
 
@@ -111,7 +42,7 @@ func main() {
 		reg := regexp.MustCompile("^#videochat")
 
 		if reg.Match([]byte(chat.Message)) {
-			client.CreateChat(url, username, password, chat.Token, fmt.Sprintf("@%v started a video call. Tap on %v to join!", chat.ActorID, jitsiURL+"/"+chat.Token))
+			bot.CreateChat(chat.Token, fmt.Sprintf("@%v started a video call. Tap on %v to join!", chat.ActorID, jitsiURL+"/"+chat.Token))
 		}
 	}
 }
--- pkg/v2/pkg/client/chat.go
+++ pkg/v2/pkg/client/chat.go
@@ -1,12 +1,5 @@
 package client
 
-import (
-	"encoding/json"
-	"path"
-
-	"github.com/go-resty/resty/v2"
-)
-
 // Chat is a chat message
 type Chat struct {
 	ID               int    `json:"id"`
@@ -21,51 +14,4 @@ type ChatResponse struct {
 	OCS struct {
 		Data []Chat `json:"data"`
 	} `json:"ocs"`
-}
-
-// GetChats gets the chat messages for a room
-func GetChats(url, username, password, room string) ([]Chat, error) {
-	client := resty.New()
-
-	res, err := client.R().
-		SetHeaders(map[string]string{
-			"OCS-APIRequest": "true",
-			"Accept":         "application/json",
-		}).
-		SetQueryParams(map[string]string{
-			"setReadMarker":  "true",
-			"lookIntoFuture": "0",
-		}).
-		SetBasicAuth(username, password).
-		Get(url + "/" + path.Join("ocs", "v2.php", "apps", "spreed", "api", "v1", "chat", room))
-	if err != nil {
-		return nil, err
-	}
-
-	var resStruct ChatResponse
-	if err := json.Unmarshal(res.Body(), &resStruct); err != nil {
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return resStruct.OCS.Data, nil
-}
-
-// CreateChat creates a chat message in a room
-func CreateChat(url, username, password, room string, message string) error {
-	client := resty.New()
-
-	_, err := client.R().
-		SetHeaders(map[string]string{
-			"OCS-APIRequest": "true",
-			"Accept":         "application/json",
-		}).
-		SetQueryParams(map[string]string{
-			"message": message,
-		}).
-		SetBasicAuth(username, password).
-		Post(url + "/" + path.Join("ocs", "v2.php", "apps", "spreed", "api", "v1", "chat", room))
-
-	return err
 }
