553168290df8cfb1c5149ac5e6308f581152c800
Felix Pojtinger <felix@pojtinger.com>
2020-04-02 22:52:43 +0200

    refactor: Decompose `Read` method into `ReadRooms` and `ReadChats`

--- pkg/v2/pkg/client/nextcloud_talk.go
+++ pkg/v2/pkg/client/nextcloud_talk.go
@@ -15,6 +15,7 @@ import (
 type NextcloudTalk struct {
 	url, username, password, dbLocation string
 	chatChan                            chan Chat
+	roomChan                            chan Room
 	statusChan                          chan string
 	knownIDs                            *pogreb.DB
 }
@@ -22,7 +23,7 @@ type NextcloudTalk struct {
 // NewNextcloudTalk creates a new Nextcloud Talk Chatbot
 func NewNextcloudTalk(url, username, password, dbLocation string, chatChan chan Chat, statusChan chan string) *NextcloudTalk {
 	return &NextcloudTalk{
-		url, username, password, dbLocation, chatChan, statusChan, nil,
+		url, username, password, dbLocation, chatChan, make(chan Room), statusChan, nil,
 	}
 }
 
@@ -94,92 +95,88 @@ func (n *NextcloudTalk) Close() error {
 	return n.knownIDs.Close()
 }
 
-// Read starts reading rooms and chats
-func (n *NextcloudTalk) Read() error {
-	go func() {
-		roomChan := make(chan Room)
-		go func() {
-			var lastRooms []Room
+// ReadRooms starts reading rooms
+func (n *NextcloudTalk) ReadRooms() error {
+	var lastRooms []Room
 
+	for {
+		rooms, err := n.getRooms()
+		if err != nil {
+			return err
+		}
+
+		for _, room := range rooms {
+			exists := false
+
+			for _, lastRoom := range lastRooms {
+				if room.ID == lastRoom.ID {
+					exists = true
+
+					break
+				}
+			}
+
+			if !exists {
+				n.roomChan <- room
+			}
+		}
+
+		lastRooms = rooms
+
+		time.Sleep(time.Second * 5)
+	}
+}
+
+// ReadChats starts reading chats
+func (n *NextcloudTalk) ReadChats() error {
+	for room := range n.roomChan {
+		n.statusChan <- fmt.Sprintf(`Joined room "%v" ("%v") with ID "%v" and token "%v"`, room.DisplayName, room.Name, room.ID, room.Token)
+
+		go func(currentRoom Room) {
 			for {
-				rooms, err := n.getRooms()
+				lastID := []byte{}
+				has, err := n.knownIDs.Has([]byte(currentRoom.Token))
 				if err != nil {
 					n.statusChan <- err.Error()
 
 					continue
 				}
 
-				for _, room := range rooms {
-					exists := false
+				if has {
+					lastID, err = n.knownIDs.Get([]byte(currentRoom.Token))
+					if err != nil {
+						n.statusChan <- err.Error()
 
-					for _, lastRoom := range lastRooms {
-						if room.ID == lastRoom.ID {
-							exists = true
-
-							break
-						}
-					}
-
-					if !exists {
-						roomChan <- room
+						continue
 					}
 				}
 
-				lastRooms = rooms
+				chats, err := n.getChats(currentRoom.Token)
+				if err != nil {
+					if err.Error() == "invalid character '<' looking for beginning of value" {
+						n.statusChan <- fmt.Sprintf(`Left room "%v" ("%v") with ID "%v" and token "%v"`, currentRoom.DisplayName, currentRoom.Name, currentRoom.ID, currentRoom.Token)
+
+						return
+					}
+
+					n.statusChan <- err.Error()
+
+					continue
+				}
+
+				chat := chats[0]
+				if strconv.Itoa(chat.ID) != string(lastID) {
+					n.chatChan <- chats[0]
+
+					if err := n.knownIDs.Put([]byte(currentRoom.Token), []byte(strconv.Itoa(chat.ID))); err != nil {
+						n.statusChan <- err.Error()
+					}
+				}
 
 				time.Sleep(time.Second * 5)
 			}
-		}()
-
-		for room := range roomChan {
-			n.statusChan <- fmt.Sprintf(`Joined room "%v" ("%v") with ID "%v" and token "%v"`, room.DisplayName, room.Name, room.ID, room.Token)
-
-			go func(currentRoom Room) {
-				for {
-					lastID := []byte{}
-					has, err := n.knownIDs.Has([]byte(currentRoom.Token))
-					if err != nil {
-						n.statusChan <- err.Error()
-
-						continue
-					}
-
-					if has {
-						lastID, err = n.knownIDs.Get([]byte(currentRoom.Token))
-						if err != nil {
-							n.statusChan <- err.Error()
-
-							continue
-						}
-					}
-
-					chats, err := n.getChats(currentRoom.Token)
-					if err != nil {
-						if err.Error() == "invalid character '<' looking for beginning of value" {
-							n.statusChan <- fmt.Sprintf(`Left room "%v" ("%v") with ID "%v" and token "%v"`, currentRoom.DisplayName, currentRoom.Name, currentRoom.ID, currentRoom.Token)
-
-							return
-						}
-
-						n.statusChan <- err.Error()
-
-						continue
-					}
-
-					chat := chats[0]
-					if strconv.Itoa(chat.ID) != string(lastID) {
-						n.chatChan <- chats[0]
-
-						if err := n.knownIDs.Put([]byte(currentRoom.Token), []byte(strconv.Itoa(chat.ID))); err != nil {
-							n.statusChan <- err.Error()
-						}
-					}
-
-					time.Sleep(time.Second * 5)
-				}
-			}(room)
-		}
-	}()
+		}(room)
+	}
 
 	return nil
 }
