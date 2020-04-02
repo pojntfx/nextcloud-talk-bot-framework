983c8a0bf553520d9a7a56d23071ed7c27d44626
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-04-02 22:55:44 +0200

    feat: Move V2 to root

--- pkg/protocol/message.go
+++
@@ -1,9 +0,0 @@
-package protocol
-
-type Message struct {
-	Id               int    `json:"id"`
-	Timestamp        int    `json:"timestamp"`
-	ActorId          string `json:"actorId"`
-	ActorDisplayName string `json:"actorDisplayName"`
-	Content          string `json:"message"`
-}
--- pkg/protocol/room.go
+++
@@ -1,6 +0,0 @@
-package protocol
-
-type Room struct {
-	Token       string  `json:"token"`
-	LastMessage Message `json:"lastMessage"`
-}
